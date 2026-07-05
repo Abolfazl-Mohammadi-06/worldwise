2467d0c3cbf38877acf5b06e9d923eee17de4cfe
AbolfazlMohammadi <abolfazl.mohammadi.85.2006@gmail.com>
2026-07-05 18:24:24 +0330

    adding  the files in components

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Pricing from "./pages/Pricing.jsx";
 import Homepage from "./pages/Homepage.jsx";
 import PageNotFound from "./pages/PageNotFound.jsx";
 import AppLayout from "./pages/AppLayout.jsx";
+import Login from "./pages/Login.jsx";
 
 export default function App() {
 
@@ -14,10 +15,11 @@ export default function App() {
                 <Route path="/" element={<Homepage/>}/>
                 <Route path="product" element={<Product/>}/>
                 <Route path="Pricing" element={<Pricing/>}/>
+                <Route path="login" element={<Login/>}/>
                 <Route path="app" element={<AppLayout/>}/>
                 <Route path="*" element={<PageNotFound/>}/>
             </Routes>
         </BrowserRouter>
     );
 }
-//E6 was fin
+//E8 was fin
