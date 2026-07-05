151c6b0fa815320fc2c653e49e149bdb20c7a48d
AbolfazlMohammadi <abolfazl.mohammadi.85.2006@gmail.com>
2026-07-05 11:34:19 +0330

    implementing Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Product from "./pages/Product.jsx";
+import Pricing from "./pages/Pricing.jsx";
+import Homepage from "./pages/Homepage.jsx";
+import PageNotFound from "./pages/PageNotFound.jsx";
+
 export default function App() {
 
 
     return (
-        <div>WorldWise</div>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Homepage/>}/>
+                <Route path="product" element={<Product/>}/>
+                <Route path="Pricing" element={<Pricing/>}/>
+                <Route path="*" element={<PageNotFound/>}/>
+            </Routes>
+        </BrowserRouter>
     );
 }
 //E3 was fin
